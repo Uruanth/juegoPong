2134a4c51d0e68c74f00e3bad831909b889a0300
Uruanth <82775623+Uruanth@users.noreply.github.com>
2021-11-19 12:17:52 -0500

    v0.0.4 Se genero la clase Bar

--- main.js
+++ main.js
@@ -71,3 +71,19 @@ class Ball {
 
     }
 };
+
+class Bar {
+    constructor(x, y, width, height, board) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+        this.speed = 5;
+    }
+
+    down = () => this.y += this.speed;
+    up = () => this.y -= this.speed;
+};
